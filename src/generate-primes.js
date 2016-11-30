aa7b5d331d83c1f9edef01c7797e435eebaee659
Will Munn <willmunn@hotmail.com>
2016-11-30 21:47:17 +0000

    Implement naive Sieve of Eratosthenes algorithm
    See https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes

--- src/generate-primes.js
+++ src/generate-primes.js
@@ -1,6 +1,41 @@
 module.exports = (howMany) => {
-    const numbers = range(howMany + 2).slice(2);
-    return numbers.slice(0, howMany);
+    const max = Math.pow(howMany, 2) + 2;
+    const firstPrime = 2;
+    return getPrimes(howMany, getInitialSeries(max), [firstPrime]);
+}
+
+function markMultiples(x) {
+    return function (possible) {
+        if (possible.number % x === 0) {
+            return {
+                number: possible.number,
+                marked: true
+            };
+        }
+        return possible;
+    }
+}
+
+function getPrimes(howMany, series, numbers) {
+    if (numbers.length === howMany) {
+        return numbers;
+    }
+    const markedSeries = series
+        .map(markMultiples(numbers.slice(-1)[0]));
+    return getPrimes(howMany, markedSeries, numbers.concat(
+        markedSeries.find(x => !x.marked).number
+    ));
+}
+
+function getInitialSeries(max) {
+    return [{
+        number: 2,
+        marked: true
+    }].concat(range(max).slice(3)
+    .map(x => ({
+        number: x,
+        marked: false
+    })));
 }
 
 function range (n) {
--- test/prime-gerneration-tests.js
+++ test/prime-gerneration-tests.js
@@ -4,14 +4,31 @@ var tape = require('tape');
 var generatePrimes = require('../src/generate-primes');
 require('babel-polyfill');
 
-tape('0 gets no primes', function(assert) {
-    var primes = generatePrimes(0);
-    assert.deepEqual(primes, []);
-    assert.end();
-});
-
 tape('1 gets 1 prime', assert => {
     const primes = generatePrimes(1);
     assert.deepEqual(primes, [2]);
+    assert.end();
+});
+
+tape('2 gets 2 primes', assert => {
+    const primes = generatePrimes(2);
+    assert.deepEqual(primes, [2, 3]);
+    assert.end();
+});
+
+function isPrime(x) {
+    for(let i = x - 1; i >= 2; i--) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+tape('5 gets 5 primes', assert => {
+    const primes = generatePrimes(5);
+    assert.equal(primes.length, 5, 'got 5 primes');
+    assert.equal(
+        true, primes.every(isPrime), 'all numbers are prime');
     assert.end();
 });
