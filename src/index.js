b903b5863cca9969aebdd40fcd1ad0a915bfde22
Will Munn <willmunn@hotmail.com>
2016-12-01 23:01:34 +0000

    Display primes on page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 const generatePrimes = require('./generate-primes');
+const getFactors = require('./get-factors');
+const table = require('./templates/table.hbs');
+const form = require('./templates/form.hbs');
+const $ = require('jquery');
+const div = $('div');
 
-generatePrimes(0);
+const formElement = $(form()).submit(displayTable);
+const formDiv = $('#form');
+formDiv.append(formElement)
+
+function displayTable (evt) {
+    const requestedNumber = Number($("#number").val());
+    const primes = generatePrimes(requestedNumber);
+    const factors = getFactors(primes);
+    $('#table').html($(table({primes, factors})));
+    evt.preventDefault();
+    return false;
+}
