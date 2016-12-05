1b1f3f64e78dd6dbb1b363a994970f6763361bc6
Will Munn <willmunn@hotmail.com>
2016-12-05 18:40:57 +0000

    Deploy

--- src/create-table.js
+++
@@ -1,7 +0,0 @@
-const table = require('./templates/table.hbs');
-
-module.exports = function createTable (data) {
-    var element = document.createElement('div');
-    element.innerHTML = table(data);
-    return element;
-}
--- src/get-factors.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (numbers) {
-    return numbers.map(n => [n].concat(numbers.map(j => n * j)));
-}
